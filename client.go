03d900e9e93b9ba304d78b46330ace4a7cabfa20
Alexander Staubo <alex@bengler.no>
2016-05-02 17:36:48 -0400

    Use go-pebbleclient.

--- client.go
+++ client.go
@@ -1,76 +1,31 @@
 package groveclient
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"net/url"
 	"strconv"
 	"strings"
-	"time"
+
+	pc "github.com/t11e/go-pebbleclient"
 )
 
-func Int(value int) *int {
-	return &value
-}
-
-func String(value string) *string {
-	return &value
-}
-
-func Bool(value bool) *bool {
-	return &value
-}
-
-type Config struct {
-	Host string
-}
-
 type Client struct {
-	Config
-	httpClient *http.Client
+	client *pc.Client
 }
 
-type Post struct {
-	Uid              string           `json:"uid"`
-	CreatedAt        time.Time        `json:"created_at"`
-	UpdatedAt        time.Time        `json:"updated_at"`
-	Document         *json.RawMessage `json:"document,omitempty"`
-	ExternalDocument *json.RawMessage `json:"external_document,omitempty"`
-	Sensitive        *json.RawMessage `json:"sensitive,omitempty"`
-	Protected        *json.RawMessage `json:"protected,omitempty"`
-	Tags             []string         `json:"tags"`
-	Deleted          bool             `json:"deleted"`
-	Published        bool             `json:"deleted"`
-	ExternalId       string           `json:"external_id"`
+func New(client *pc.Client) (*Client, error) {
+	return &Client{client}, nil
 }
 
-type DocumentAttributes map[string]interface{}
-
-func (post *Post) GetDocument() (DocumentAttributes, error) {
-	return post.extractDocumentAttributes(post.Document)
-}
-
-func (post *Post) GetExternalDocument() (DocumentAttributes, error) {
-	return post.extractDocumentAttributes(post.ExternalDocument)
-}
-
-func (post *Post) GetSensitive() (DocumentAttributes, error) {
-	return post.extractDocumentAttributes(post.Sensitive)
-}
-
-func (post *Post) GetProtected() (DocumentAttributes, error) {
-	return post.extractDocumentAttributes(post.Protected)
-}
-
-func (post *Post) extractDocumentAttributes(raw *json.RawMessage) (DocumentAttributes, error) {
-	var attrs DocumentAttributes
-	if raw != nil {
-		if err := json.Unmarshal(*raw, &attrs); err != nil {
-			return nil, err
-		}
+// NewFromRequest constructs a new client from an HTTP request.
+func NewFromRequest(options pc.ClientOptions, req *http.Request) (*Client, error) {
+	if options.AppName == "" {
+		options.AppName = "grove"
 	}
-	return attrs, nil
+	client, err := pc.NewFromRequest(options, req)
+	if err != nil {
+		return nil, err
+	}
+	return &Client{client}, nil
 }
 
 type GetOptions struct {
@@ -86,19 +41,12 @@ type GetOutput struct {
 	Posts []GetResultItem `json:"posts"`
 }
 
-func NewClient(config Config) *Client {
-	return &Client{config, &http.Client{
-		Timeout: 60 * time.Second,
-	}}
-}
-
 func (client *Client) GetMany(uids []string, options GetOptions) (*GetOutput, error) {
-	queryString := make(url.Values)
-	if options.Raw != nil && *options.Raw == false {
-		queryString["raw"] = []string{"false"}
+	params := pc.Params{
+		"raw": options.Raw != nil && *options.Raw,
 	}
 	if options.Limit != nil {
-		queryString["limit"] = []string{strconv.Itoa(*options.Limit)}
+		params["limit"] = []string{strconv.Itoa(*options.Limit)}
 	}
 
 	uidList := strings.Join(uids, ",")
@@ -106,25 +54,10 @@ func (client *Client) GetMany(uids []string, options GetOptions) (*GetOutput, er
 		uidList = uidList + ","
 	}
 
-	url := fmt.Sprintf("http://%s/api/grove/v1/posts/%s?%s",
-		client.Host, uidList, queryString.Encode())
-
-	req, err := http.NewRequest("GET", url, nil)
+	var out GetOutput
+	err := client.client.Get(uidList, &params, &out)
 	if err != nil {
 		return nil, err
 	}
-
-	resp, err := client.httpClient.Do(req)
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("Request [GET %s] failed with status code %d: %s",
-			url, resp.StatusCode, resp.Status)
-	}
-
-	var output GetOutput
-	reader := json.NewDecoder(resp.Body)
-	if err := reader.Decode(&output); err != nil {
-		return nil, err
-	}
-
-	return &output, nil
+	return &out, err
 }
---
+++ structs.go
@@ -0,0 +1,60 @@
+package groveclient
+
+import (
+	"encoding/json"
+	"time"
+)
+
+func Int(value int) *int {
+	return &value
+}
+
+func String(value string) *string {
+	return &value
+}
+
+func Bool(value bool) *bool {
+	return &value
+}
+
+type DocumentAttributes map[string]interface{}
+
+type Post struct {
+	Uid              string           `json:"uid"`
+	CreatedAt        time.Time        `json:"created_at"`
+	UpdatedAt        time.Time        `json:"updated_at"`
+	Document         *json.RawMessage `json:"document,omitempty"`
+	ExternalDocument *json.RawMessage `json:"external_document,omitempty"`
+	Sensitive        *json.RawMessage `json:"sensitive,omitempty"`
+	Protected        *json.RawMessage `json:"protected,omitempty"`
+	Tags             []string         `json:"tags"`
+	Deleted          bool             `json:"deleted"`
+	Published        bool             `json:"deleted"`
+	ExternalId       string           `json:"external_id"`
+}
+
+func (post *Post) GetDocument() (DocumentAttributes, error) {
+	return post.extractDocumentAttributes(post.Document)
+}
+
+func (post *Post) GetExternalDocument() (DocumentAttributes, error) {
+	return post.extractDocumentAttributes(post.ExternalDocument)
+}
+
+func (post *Post) GetSensitive() (DocumentAttributes, error) {
+	return post.extractDocumentAttributes(post.Sensitive)
+}
+
+func (post *Post) GetProtected() (DocumentAttributes, error) {
+	return post.extractDocumentAttributes(post.Protected)
+}
+
+func (post *Post) extractDocumentAttributes(raw *json.RawMessage) (DocumentAttributes, error) {
+	var attrs DocumentAttributes
+	if raw != nil {
+		if err := json.Unmarshal(*raw, &attrs); err != nil {
+			return nil, err
+		}
+	}
+	return attrs, nil
+}
