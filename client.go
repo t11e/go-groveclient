1df1d60a7803a375e3c8de146338c18865bd7c9b
Alexander Staubo <alex@bengler.no>
2016-05-20 14:24:26 -0400

    Update to latest go-pebbleclient.

--- client.go
+++ client.go
@@ -30,10 +30,11 @@ func New(client pc.Client) (*Client, error) {
 func (client *Client) Get(uid string, options GetOptions) (*PostItem, error) {
 	params := pc.Params{
 		"raw": options.Raw != nil && *options.Raw,
+		"uid": uid,
 	}
 
 	var out PostItem
-	err := client.c.Get(pc.FormatPath("/posts/:uid", pc.Params{"uid": uid}), &pc.RequestOptions{
+	err := client.c.Get("/posts/:uid", &pc.RequestOptions{
 		Params: params,
 	}, &out)
 	if err != nil {
@@ -43,20 +44,21 @@ func (client *Client) Get(uid string, options GetOptions) (*PostItem, error) {
 }
 
 func (client *Client) GetMany(uids []string, options GetManyOptions) (*GetManyOutput, error) {
-	params := pc.Params{
-		"raw": options.Raw != nil && *options.Raw,
-	}
-	if options.Limit != nil {
-		params["limit"] = *options.Limit
-	}
-
 	uidList := strings.Join(uids, ",")
 	if len(uids) > 1 {
 		uidList = uidList + ","
 	}
 
+	params := pc.Params{
+		"raw":  options.Raw != nil && *options.Raw,
+		"uids": uidList,
+	}
+	if options.Limit != nil {
+		params["limit"] = *options.Limit
+	}
+
 	var out GetManyOutput
-	err := client.c.Get(pc.FormatPath("/posts/:uids", pc.Params{"uids": uidList}), &pc.RequestOptions{
+	err := client.c.Get("/posts/:uids", &pc.RequestOptions{
 		Params: params,
 	}, &out)
 	if err != nil {
